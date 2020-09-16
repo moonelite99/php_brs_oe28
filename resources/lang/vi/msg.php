3ac3a4f0d76082b3cb96cc39d626bcca57ef12f8
moonelite99 <quan.nb173315@sis.hust.edu.vn>
2020-09-16 13:32:45 +0700

    Auth

---
+++ resources/lang/vi/msg.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'home' => 'Trang chủ',
+    'category' => 'Thể loại',
+    'book' => 'Sách',
+    'review' => 'Nhận xét',
+    'contact' => 'Liên hệ',
+    'login' => 'Đăng nhập',
+    'trending_book' => 'SÁCH THỊNH HÀNH',
+    'lastest_reviews' => 'NHẬN XÉT GẦN ĐÂY',
+    'literary' => 'Văn học',
+    'economic' => 'Kinh tế',
+    'historical' => 'Lịch sử',
+    'religional' => 'Tôn giáo',
+    'dictionary' => 'Từ điển',
+    'comic' => 'Truyện tranh',
+    'username' => 'Tên đăng nhập',
+    'password' => 'Mật khẩu',
+    'remember_me' => 'Ghi nhớ đăng nhập',
+    'forgot_pass' => 'Quên mật khẩu?',
+    'register' => 'Đăng kí',
+    'email' => 'Email',
+    'name' => 'Tên',
+    'logout' => 'Đăng xuất',
+    'email_address' => 'Địa chỉ Email',
+    'pass_confirm' => 'Nhập lại mật khẩu',
+];
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="padding-bottom-350">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header">{{ trans('msg.login') }}</div>
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group-b row">
+                                    <label for="username"
+                                        class="col-md-4 col-form-label text-md-right">{{ trans('msg.username') }}</label>
+                                    <div class="col-md-6">
+                                        <input id="username" type="text"
+                                            class="form-control-b @error ('username') is-invalid @enderror" name="username"
+                                            value="{{ old('username') }}" required autocomplete="username" autofocus>
+                                        @error ('username')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="form-group-b row">
+                                    <label for="password"
+                                        class="col-md-4 col-form-label text-md-right">{{ trans('msg.password') }}</label>
+                                    <div class="col-md-6">
+                                        <input id="password" type="password"
+                                            class="form-control-b @error ('password') is-invalid @enderror" name="password"
+                                            required>
+                                        @error ('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="form-group-b row">
+                                    <div class="col-md-6 offset-md-4">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                                                {{ old('remember') ? 'checked' : '' }}>
+                                            <label class="form-check-label" for="remember">
+                                                {{ trans('msg.remember_me') }}
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group-b row mb-0">
+                                    <div class="col-md-8 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ trans('msg.login') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
